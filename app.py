eec1e92e1c424d4a5b7ce51fefd0a2c415f53961
yimmyflorez <yimmyflorez16@hotmail.com>
2024-08-10 17:26:48 +1000

    initial commit

---
+++ app.py
@@ -0,0 +1,32 @@
+import scipy.stats
+import streamlit as st
+import time
+
+st.header('Lanzar una moneda')
+
+chart = st.line_chart([0.5])
+
+def toss_coin(n):
+
+    trial_outcomes = scipy.stats.bernoulli.rvs(p=0.5, size=n)
+
+    mean = None
+    outcome_no = 0
+    outcome_1_count = 0
+
+    for r in trial_outcomes:
+        outcome_no +=1
+        if r == 1:
+            outcome_1_count += 1
+        mean = outcome_1_count / outcome_no
+        chart.add_rows([mean])
+        time.sleep(0.05)
+
+    return mean
+
+number_of_trials = st.slider('¿Número de intentos?', 1, 1000, 10)
+start_button = st.button('Ejecutar')
+
+if start_button:
+    st.write(f'Experimento con {number_of_trials} intentos en curso.')
+    mean = toss_coin(number_of_trials)
